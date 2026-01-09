4dd268742b59b9cf2869a8eb667cb397ace79fc8
jinoc <choiae369@gmail.com>
2026-01-09 16:18:40 +0900

    design: 헤더 추가

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@/app/widgets/header';
 import { type ReactNode } from 'react';
 
 type Props = {
@@ -6,8 +7,9 @@ type Props = {
 
 export const MainLayout = ({ children }: Props) => {
   return (
-    <main className="min-h-screen w-full bg-linear-to-br from-blue-400 via-sky-300 to-indigo-200 flex justify-center items-center p-4 sm:p-8">
-      <div className="w-full h-[850px] bg-white/30 backdrop-blur-xl rounded-[40px] shadow-2xl border border-white/40 overflow-hidden relative text-slate-700 flex flex-col">
+    <main className="min-h-screen w-full bg-linear-to-br from-blue-400 via-sky-300 to-indigo-200 flex justify-center items-center p-4">
+      <div className="w-full h-full bg-white/30 backdrop-blur-xl rounded-[40px] shadow-2xl border border-white/40 overflow-hidden relative text-slate-700 flex flex-col">
+        <Header currentPlace="서울 종로구" />
         {children}
       </div>
     </main>
--- src/app/widgets/header/Header.tsx
+++ src/app/widgets/header/Header.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import { MapPin, Menu } from 'lucide-react';
 
-export const Header = () => {
-  return <div>Header</div>;
+type Props = {
+  currentPlace: string;
+};
+
+export const Header = ({ currentPlace }: Props) => {
+  return (
+    <header className="flex justify-between items-center p-6 pt-8">
+      <button className="p-2 bg-white/40 rounded-full hover:bg-white/60 transition">
+        <Menu size={24} className="text-slate-700" />
+      </button>
+      <div className="flex items-center gap-2">
+        <MapPin size={18} className="text-blue-600" />
+        <span className="font-bold text-lg">{currentPlace}</span>
+      </div>
+    </header>
+  );
 };
