862472d978c7b2b3578e588d06f67448156985dd
jinoc <choiae369@gmail.com>
2026-01-13 18:17:04 +0900

    refactor: 커스텀 axios instance를 사용하도록 변경

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { type AxiosRequestConfig, type AxiosResponse } from 'axios';
 
 export const weatherApi = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -10,3 +10,22 @@ export const weatherApi = axios.create({
     dataType: 'JSON',
   },
 });
+
+weatherApi.interceptors.response.use(
+  (response) => {
+    // header의 결과 코드 등으로 에러 처리가 필요할 수 있음
+    const { body } = response?.data?.response || {};
+
+    return body || {};
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const getWeatherApi = async <T>(
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  return weatherApi.get<AxiosResponse<T>, T>(url, config);
+};
